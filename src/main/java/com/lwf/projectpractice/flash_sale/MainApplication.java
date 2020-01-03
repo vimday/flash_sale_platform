6b803ed8f7462e77458a16441efaf5b52ada3d5e
vimday <lwfplus@gmail.com>
2020-01-03 22:30:14 +0800

    init

---
+++ src/main/java/com/lwf/projectpractice/flash_sale/MainApplication.java
@@ -0,0 +1,13 @@
+package com.lwf.projectpractice.flash_sale;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MainApplication {
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(MainApplication.class, args);
+    }
+}
