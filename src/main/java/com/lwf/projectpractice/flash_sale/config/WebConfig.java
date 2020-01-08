102f2448f53601e5e2bd7efe7633ea5d9e73d5f6
vimday <lwfplus@gmail.com>
2020-01-08 12:00:18 +0800

    优化WebConfig实现

--- src/main/java/com/lwf/projectpractice/flash_sale/config/WebConfig.java
+++ src/main/java/com/lwf/projectpractice/flash_sale/config/WebConfig.java
@@ -1,13 +1,21 @@
 package com.lwf.projectpractice.flash_sale.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-public class WebConfig extends WebMvcConfigurerAdapter {
+import java.util.List;
 
-    @Autowired
-    UserArgumentResolver userArgumentResolver;
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+
+    private final UserArgumentResolver userArgumentResolver;
+
+    @Autowired public WebConfig(UserArgumentResolver userArgumentResolver){
+        this.userArgumentResolver=userArgumentResolver;
+    }
 
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> argumentResolvers) {
         argumentResolvers.add(userArgumentResolver);
